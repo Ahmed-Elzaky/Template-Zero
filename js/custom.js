8deb3a6bc63f32c20a95c76dcdd216255c232853
Ahmed-Elzaky <A7med3lzeky@outlook.com>
2022-10-29 14:57:10 +0200

    uplaod all files

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+/* Global $ */
+
+$(function () {
+  'use strict';
+  $('.info-list li').click(function () {
+    $(this).addClass('selected').siblings('li').removeClass('selected');
+    $('.info-content > div').hide();
+    $('.' + $(this).data('class')).fadeIn();
+  });
+
+  $('.nav-list li').click(function () {
+    $(this).addClass('selected').siblings('li').removeClass('selected');
+    $('.' + $(this).data('class')).addClass('bg-d').siblings('div').removeClass('bg-d');
+
+
+  });
+
+  $('.burger').click(function () {
+    $(this).toggleClass('bgi');
+    $('.nav-list').toggleClass('show');
+  });
+
+});
+
+
