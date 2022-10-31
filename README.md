ef8cad14330bf175917c2e0a1df9c5dfd044949a
Ahmed-Elzaky <A7med3lzeky@outlook.com>
2022-10-31 16:17:47 +0200

    modified all files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # responsive-design-elzaky
 #### Demo
 https://ahmed-elzaky.github.io/responsive-design-elzaky/
+
+Thanks to Elzero Web School.
--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,51 @@
 /* Global $ */
 
-$(function () {
-  'use strict';
-  $('.info-list li').click(function () {
-    $(this).addClass('selected').siblings('li').removeClass('selected');
-    $('.info-content > div').hide();
-    $('.' + $(this).data('class')).fadeIn();
-  });
 
-  $('.nav-list li').click(function () {
-    $(this).addClass('selected').siblings('li').removeClass('selected');
-    $('.' + $(this).data('class')).addClass('bg-d').siblings('div').removeClass('bg-d');
+'use strict';
 
+$('.info-list li').click(function () {
+  $(this).addClass('selected').siblings('li').removeClass('selected');
+  $('.info-content > div').hide();
+  $('.' + $(this).data('class')).fadeIn();
+})
 
-  });
-
-  $('.burger').click(function () {
-    $(this).toggleClass('bgi');
-    $('.nav-list').toggleClass('show');
-    $('.burger .').toggleClass('show');
-  });
-
-});
+$('.nav-list li').click(function () {
+  $(this).addClass('selected').siblings('li').removeClass('selected')
+  // $('.' + $(this).data('class')).addClass('bg-d').siblings('div').removeClass('bg-d');
+  let offsetSec = $('.' + $(this).data('class')).offset().top - 20
+  $('body, html').animate ({
+    scrollTop: offsetSec,
+  }, 1000)
 
 
+})
+
+/* $('body').click(function () {
+  $('.nav-list').removeClass('show');
+}) */
+
+$('.burger').click(function () {
+  $(this).toggleClass('bgi');
+  $('.nav-list').toggleClass('show');
+  // $('.burger .').toggleClass('show');
+})
+
+$(window).scroll(function () {
+  let scrolly = $(window).scrollTop();
+  // window.console.log(scrolly);
+
+  /* if(scrolly >= 554.5555419921875) 
+  if (scrolly >= $('.features').offset().top - 50) 
+  if (scrolly >= $('.header').height() / 3) */
+
+  if (scrolly >= $('.header').height() / 3) {
+    // window.console.log('ahmed');
+    $('.navbar').addClass('navbar-scroll');
+  }
+  else {
+    $('.navbar').removeClass('navbar-scroll');
+  }
+
+  $('.nav-list').removeClass('show'); // hide .nav-list if open while scroll
+
+})
